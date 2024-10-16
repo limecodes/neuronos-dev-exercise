b2b7b16cb75923d3fef5dc6fba1cd807a589e79e
limecodes <yan.shcherbakov+limecodes@gmail.com>
2024-10-16 14:26:16 +0300

    feat(Background): Added event callbacks to events for background process.

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,5 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('onInstalled....')
-})
+import Events from './events'
+
+chrome.alarms.onAlarm.addListener(Events.onAlarm)
+chrome.runtime.onInstalled.addListener(Events.onInstalled)
+chrome.runtime.onMessage.addListener(Events.onMessage)
