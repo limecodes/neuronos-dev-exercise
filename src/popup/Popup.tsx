a5a77ef748230d7476947a4da49459057dccbd48
limecodes <yan.shcherbakov+limecodes@gmail.com>
2024-10-16 14:31:27 +0300

    feat(Popup): Adding classes for component.

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,7 +1,11 @@
 export default function Popup() {
   return (
-    <div>
-      <h1>Popup</h1>
+    <div className="popupContainer" style={{ width: 800, height: 800 }}>
+      <div className="container mx-auto p-4">
+        <div className="flex justify-between items-center mb-4">
+          <h1 className="text-xl font-semibold">Messages</h1>
+        </div>
+      </div>
     </div>
   )
 }
