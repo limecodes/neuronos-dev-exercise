e801a409666fc8b127024f2c0748c0b48c4efdf3
limecodes <yan.shcherbakov+limecodes@gmail.com>
2024-10-16 15:39:29 +0300

    fix: Forgot to remove imports from eslint config.

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,6 @@
 import js from '@eslint/js'
-import eslintImport from 'eslint-plugin-import'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
@@ -18,8 +16,6 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      'simple-import-sort': simpleImportSort,
-      import: eslintImport,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
